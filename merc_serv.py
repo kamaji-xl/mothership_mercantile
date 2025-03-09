385c8e97b9a85e326893c84bdef4298f231f9453
kamaji-xl <kamajixl@gmail.com>
2025-03-09 03:07:22 -0600

    Integrated remaining microservices.

--- merc_serv.py
+++ merc_serv.py
@@ -9,7 +9,6 @@ KCR = 'kcr'
 
 def apply_cost_mod(cost):
     cost, mod = parse_cost(cost)
-    print(cost, mod)
 
     if mod == KCR:
         cost = int(cost) * 1000
@@ -24,46 +23,99 @@ def parse_cost(cost):
     return None
 
 
-def buy_item(req, log):
+def complete_transaction(req, log):
+    command = req['command']
     item_name = req['item_name']
     qty = req['qty']
     cost = req['cost']
-    print(cost)
     cost = apply_cost_mod(cost)
     char_name = req['char_name']
     starting_balance = req['balance']
+    new_balance = 0
     date_time = datetime.now()
+    total_cost = int(qty) * int(cost)
 
-    new_balance = int(starting_balance) - int(cost) * int(qty)
+    if command == "buy":
+        cost = -int(cost)
+        new_balance = int(starting_balance) + int(cost) * int(qty)
+        total_cost = -total_cost
+        if new_balance < 0:
+            return {
+                "status": "Insufficient funds",
+            }
+    elif command == "sell":
+        new_balance = int(starting_balance) + int(cost) * int(qty)
 
     if char_name not in log:
-        log[char_name] = {'transaction_count':0, 'transactions': []}
+        log[char_name] = {'transaction_count': 0, 'transactions': []}
 
-    transaction_number = log[char_name]['transaction_count']
     log[char_name]['transaction_count'] += 1
 
     transaction_details = {
+        "number": log[char_name]['transaction_count']-1,
         "item": item_name,
+        "action": command,
         "qty": qty,
         "cost_per_item": cost,
-        "total_cost": cost * qty,
+        "total_cost": total_cost,
         "pre_balance": starting_balance,
         "post_balance": new_balance,
-        "date": str(date_time)
+        "date": date_time.strftime("%m-%d-%Y"),
     }
 
     log[char_name]['transactions'].append(transaction_details)
+    transaction_details.update({"status": "success"})
 
     return transaction_details
 
 
 def merc_req_handler(req, log):
-    if req["command"] == "buy":
-        res = buy_item(req, log)
-    elif req["command"] == "sell":
-        pass
+    if req["command"] == "buy" or req["command"] == "sell":
+        res = complete_transaction(req, log)
+    elif req["command"] == "pull_hist":
+        res = pull_history(req, log)
     else:
         res = {"status": "error"}
+
+    return res
+
+
+def print_transactions(details):
+    for key, value in details.items():
+        print(f"{key}: {value}")
+
+
+def pull_history(req, log):
+    char_name = req['char_name']
+    t_type = req['type']
+
+    if t_type == 'all':
+        try:
+            t_list = []
+            for txn in log[char_name]['transactions']:
+                transaction = [txn['number'], txn['item'], txn['action'], txn['qty'], txn['cost_per_item'],
+                               txn['total_cost'], txn['pre_balance'], txn['post_balance'], txn['date']]
+                t_list.append(transaction)
+            res = {"status": "success", "transactions": t_list}
+
+        except KeyError as e:
+            print(f"KeyError: {e}")
+            res = {"status": "error"}
+    else:
+        try:
+            t_list = []
+            print(t_type)
+            for txn in log[char_name]['transactions']:
+                print("test", txn)
+                if txn['item'] == t_type:
+                    transaction = [txn['number'], txn['item'], txn['action'], txn['qty'], txn['cost_per_item'],
+                                   txn['total_cost'], txn['pre_balance'], txn['post_balance'], txn['date']]
+                    t_list.append(transaction)
+            res = {"status": "success", "transactions": t_list}
+
+        except KeyError as e:
+            print(f"KeyError: {e}")
+            res = {"status": "error"}
 
     return res
 
@@ -80,7 +132,9 @@ if __name__ == "__main__":
     while True:
         try:
             request_json = socket.recv_json()
-            print(request_json)
+            print("Received request:")
+            for key, value in request_json.items():
+                print(f"\t{key}: {value}")
             response = merc_req_handler(request_json, transaction_log)
             print(f"\nsending response:")
             for key in response.keys():
